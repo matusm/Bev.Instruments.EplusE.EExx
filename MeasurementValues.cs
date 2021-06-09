f895f9c6bcc130d2926cf1ebc87dcf3cf14fce93
Michael Matus <michael@matus.at>
2021-06-09 19:38:14 +0200

    Class MeasurementValues made comparable

--- MeasurementValues.cs
+++ MeasurementValues.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 
-namespace Bev.Instruments.EplusE.EExx
-{
-    public class MeasurementValues
-    {
+namespace Bev.Instruments.EplusE.EExx {
+    public class MeasurementValues : IComparable<MeasurementValues> {
+
+        public DateTime TimeStamp { get; }
         public double Temperature { get; }
         public double Humidity { get; }
-        public DateTime TimeStamp { get; }
 
-        public MeasurementValues(double temperature, double humidity)
-        {
+        public MeasurementValues(double temperature, double humidity) {
             TimeStamp = DateTime.UtcNow;
             Temperature = temperature;
             Humidity = humidity;
         }
 
+        public int CompareTo(MeasurementValues other) {
+            return TimeStamp.CompareTo(other.TimeStamp);
+        }
     }
 }
